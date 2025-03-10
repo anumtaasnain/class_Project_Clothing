eb719fe7cd085a96cada728bde846b3a282676bd
Your Name <you@example.com>
2025-03-10 09:11:12 +0500

    Product Page Created

---
+++ js/script.js
@@ -0,0 +1,53 @@
+// Function for image changed and reset on hover:
+function imgchng1() {
+    document.getElementById("myimg1").setAttribute("src", "Images/d1.webp");
+}
+function imgreset1() {
+    document.getElementById("myimg1").setAttribute("src", "Images/d3.webp");
+}
+function imgchng2() {
+    document.getElementById("myimg2").setAttribute("src", "Images/d7.webp");
+}
+function imgreset2() {
+    document.getElementById("myimg2").setAttribute("src", "Images/d5.webp");
+}
+function imgchng3() {
+    document.getElementById("myimg3").setAttribute("src", "Images/d11.webp");
+}
+function imgreset3() {
+    document.getElementById("myimg3").setAttribute("src", "Images/d9.webp");
+}
+function imgchng4() {
+    document.getElementById("myimg4").setAttribute("src", "Images/d16.webp");
+}
+function imgreset4() {
+    document.getElementById("myimg4").setAttribute("src", "Images/d15.webp");
+}
+function imgchng5() {
+    document.getElementById("myimg5").setAttribute("src", "Images/d19.webp");
+}
+function imgreset5() {
+    document.getElementById("myimg5").setAttribute("src", "Images/d18.webp");
+}
+function imgchng6() {
+    document.getElementById("myimg6").setAttribute("src", "Images/d21.webp");
+}
+function imgreset6() {
+    document.getElementById("myimg6").setAttribute("src", "Images/d20.webp");
+}
+function imgchng7() {
+    document.getElementById("myimg7").setAttribute("src", "Images/d24.webp");
+}
+function imgreset7() {
+    document.getElementById("myimg7").setAttribute("src", "Images/d23.webp");
+}
+function imgchng8() {
+    document.getElementById("myimg8").setAttribute("src", "Images/d26.webp");
+}
+function imgreset8() {
+    document.getElementById("myimg8").setAttribute("src", "Images/d25.webp");
+}
+
+function addToCart() {
+    alert("Item added to cart successfully!");
+}
